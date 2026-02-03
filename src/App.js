745edb34aeab4f7f1e65762be1ebd2c97c46f055
Perlaulicticmethodick <sergioroga2005@gmail.com>
2026-02-03 10:23:33 +0100

    list of tasks

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import { useState,useRef,useEffect } from 'react';
 import './App.css';
 
 function App() {
+  let[task,setTask]=useState([
+  {
+    name:"Tarea1",
+    place:"School",
+    prioridad:0,
+
+  },
+  {
+    name:"Tarea2",
+    place:"School",
+    prioridad:0,
+  },
+])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {task.map(t=>
+         <li>
+          <b>{t.name}</b>
+          <div>prioridad:{t.prioridad}</div>
+          <div>Lugar:{t.place}</div>
+         </li>)}
     </div>
   );
 }
 
 export default App;
+//codigo jsx entre java y html
