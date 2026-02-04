bf06eb57942ffff0cd58e66448aba8f3df251210
Perlaulicticmethodick <sergioroga2005@gmail.com>
2026-02-04 08:27:36 +0100

    trivial message

--- src/Components/questions.js
+++ src/Components/questions.js
@@ -4,20 +4,29 @@ let Questions=(props)=>{
     
     let {question,setpoints,points}=props;
     let[numberofquest,setnumber]=useState(0)
+    let[message,setmessage]=useState(null);
     let anwserQuest=(anwser)=>{
         if(anwser=question[numberofquest].correct){
             //points up
+            setmessage("Respuesta correcta!")
             setpoints(points+1)
-
+            
 
 
         }else{
             //mal
+            setmessage("Respuesta incorrecta!")
         }
         if(numberofquest<question.length-1){
             setnumber(numberofquest+1)
+            setTimeout(()=>{
+                setmessage(null)
+            },2000)
         }else{
             //se acabo
+            setTimeout(()=>{
+                setmessage("SE ACABO!")
+            },2000)
             setTimeout(()=>{
                 setnumber(0)
                 setpoints(0)
@@ -29,6 +38,9 @@ let Questions=(props)=>{
     }
     return(
         <div className="question">
+            {message!=null &&
+                <h2>{message}</h2>
+            }
             <div>
                 <h2>Question {numberofquest+1}</h2>
                 <p id="questionText">{question[numberofquest].text}</p>
